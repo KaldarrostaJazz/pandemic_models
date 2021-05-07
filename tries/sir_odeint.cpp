0a2d8c9ca8d57f2c76012ae93f290dac365d064f
Giuseppe Sguera <giuseppe.sguera2@studio.unibo.it>
2021-05-07 21:41:00 +0200

    modified sir_odeint.cpp

--- tries/sir_odeint.cpp
+++ tries/sir_odeint.cpp
@@ -1,22 +1,23 @@
 #include <boost/array.hpp>
 #include <boost/numeric/odeint.hpp>
 
-double const beta = 0.1;
-double const gamma = 0.05;
+double const b = 0.1;
+double const g = 0.05;
+int const N = 100;
 
-typedef boost::array<double , 3> state;
+typedef boost::array<double, 3> state;
 
-void sir(state const& x, state const& dxdt, double t) {
-	dxdt[0] = -beta * x[0] * x[1];
-	dxdt[1] = beta * x[0] * x[1] - gamma*x[1];
-	dxdt[2] = gamma * x[1];
+void sir(state const& x, state  &dxdt, double t) {
+  dxdt[0] = -(b / N) * x[0] * x[1];
+  dxdt[1] = (b / N) * x[0] * x[1] - g * x[1];
+  dxdt[2] = g * x[1];
 }
 
 void print_sir(state const& x, double const t) {
-	std::cout << t << " " << x[0] << " " << x[1] << " " << x[2] << '\n';
+  std::cout << t << " " << x[0] << " " << x[1] << " " << x[2] << '\n';
 }
 
 int main() {
-	state x = {0.99, 0.01, 0.0}; //intial conditions
-	boost::numeric::odeint::integrate(sir, x, 0.0, 200.0, 0.1, print_sir);
+  state x = {99., 1., 0.};  // intial conditions
+  boost::numeric::odeint::integrate(sir, x, 0.0, 200.0, 0.1, print_sir);
 }
