208a65e7f62ba32c94c517dbb9a88e184a1cb55a
Giuseppe Sguera <giuseppe.sguera2@studio.unibo.it>
2021-05-10 10:14:41 +0200

    added stocastic_sir_model

---
+++ stocastic_sir_model/sir_stocastic.cpp
@@ -0,0 +1,49 @@
+#include <boost/array.hpp>
+
+#include <iostream>
+#include <fstream>
+#include <ctime>
+#include <random>
+int main() {
+  double const beta = 0.05;
+  double const gamma = 0.1;
+  double const dt = 0.001;
+  double const time = 10;
+  double const b = beta * dt;
+  double const g = gamma * dt;
+  int const n = 100;
+  int const num = time / dt;
+  boost::array<double, num> s;
+  boost::array<double, num> k;
+  boost::array<double, num> r;
+  k[0] = 5;
+  s[0] = 95;
+  r[0] = 0;
+  //time_t t;
+  std::random_device rd;
+  std::mt19937 mt(rd()/*time(&t)*/);
+  std::uniform_real_distribution<> v(0.0, 1.0);
+  for (int i = 1; i < num; ++i) {
+    double c = v(mt);
+    if (c < (b * k[i - 1] * s[i - 1])) {
+      k[i] = k[i - 1] + 1;
+      s[i] = s[i - 1] - 1;
+      r[i] = r[i - 1];
+    } else if (c >= (1 - g * k[i - 1])) {
+      r[i] = r[i - 1] + 1;
+      k[i] = k[i - 1] - 1;
+      s[i] = s[i - 1];
+    } else {
+      k[i] = k[i - 1];
+      s[i] = s[i - 1];
+      r[i] = r[i - 1];
+    }
+  }
+  std::ofstream output_file;
+  output_file.open("dati.dat");
+  for (int q = 0; q != num; ++q) {
+  std::cout << q << " " << s[q] << " " << k[q] << " " << r[q] << '\n';
+  output_file << q << " " << s[q] << " " << k[q] << " " << r[q] << '\n';
+  }
+  output_file.close();
+}
