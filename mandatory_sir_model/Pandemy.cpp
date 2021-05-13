03550cb444e1167ce9651e13ad9f760eb3a8cdab
Giuseppe Sguera <giuseppe.sguera2@studio.unibo.it>
2021-05-13 15:58:05 +0200

    modified

--- mandatory_sir_model/Pandemy.cpp
+++ mandatory_sir_model/Pandemy.cpp
@@ -1,21 +1,32 @@
 #include "Pandemy.hpp"
 
-#include <cassert>
 #include <cmath>
-
+#include <fstream>
 std::vector<State> Pandemy::progression(int D) const {
   std::vector<State> result{state};
-  double const beta = virus.beta;
-  double const gamma = virus.gamma;
+  double beta = virus.beta;
+  double gamma = virus.gamma;
   for (int day = 1; day != D; ++day) {
-    auto const last = result.back();
+    auto const prev = result.back();
     State s{};
-    s.S = last.S - round((beta / people) * last.S * last.I);
-    s.I = last.I + round((beta / people) * last.S * last.I) -
-          round(gamma * last.I);
-    s.R = last.R + round(gamma * last.I);
-    assert(people == s.S + s.R + s.I);
+    s.S = prev.S - round((beta / people) * prev.S * prev.I);
+    s.I = prev.I + round((beta / people) * prev.S * prev.I) -
+          round(gamma * prev.I);
+    s.R = prev.R + round(gamma * prev.I);
     result.push_back(s);
   }
   return result;
+}
+std::vector<State> Pandemy::get_data(std::string& file) const {
+  std::vector<State> pandemic_data{};
+  int day, S, I, R;
+  std::ifstream data_file;
+  data_file.open(file);
+  while (data_file.good()) {
+    data_file >> day >> S >> I >> R;
+    State s = {S, I, R};
+    pandemic_data.push_back(s);
+  }
+  data_file.close();
+  return pandemic_data;
 }
--- mandatory_sir_model/Pandemy.hpp
+++ mandatory_sir_model/Pandemy.hpp
@@ -12,17 +12,17 @@ class Pandemy {
   Virus virus;
 
  public:
-  Pandemy(int const people, State const& state, Virus const& virus)
+  Pandemy(int const people = 0, State const& state = {0, 0, 0}, Virus const& virus = {0.0, 0.0}) 
       : people{people}, state{state}, virus{virus} {
     if (state.S < 0 || state.I < 0 || state.R < 0) {
-      throw std::range_error("S, I and R must be positive integer values!");
+      throw std::range_error("Insert positive values!");
     } else if (people != state.S + state.I + state.R) {
       throw std::range_error("It must be N = S + I + R");
-    } else if (virus.beta < 0. || virus.beta > 1. || virus.gamma < 0. ||
-               virus.gamma > 1.) {
-      throw std::range_error("Beta and gamma must be in the range [0. ; 1.]");
+    } else if (virus.beta < 0. || virus.gamma < 0.) {
+      throw std::range_error("Insert positive values!");
     }
   }
   std::vector<State> progression(int D) const;
+  std::vector<State> get_data(std::string& file) const;
 };
 #endif
