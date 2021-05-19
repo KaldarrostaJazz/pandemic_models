dfe840e01e287d26ccbad3231af2c508be9a80e3
Giuseppe Sguera <giuseppe.sguera2@studio.unibo.it>
2021-05-19 21:52:28 +0200

    formatted

--- mandatory_sir_model/main.cpp
+++ mandatory_sir_model/main.cpp
@@ -7,32 +7,48 @@
 
 int main(int argc, const char** argv) {
   std::string option;
-	auto cli = lyra::cli();
-	cli.add_argument(
-			lyra::opt(option, "running_option")
-			.name("-o")
-			.name("--opt")
-			.help("Choose if fit_mode or sir_mode"));
-	auto pippo_inzaghi = cli.parse({argc, argv});
+  auto cli = lyra::cli();
+  cli.add_argument(lyra::opt(option, "running_option")
+                       .name("-o")
+                       .name("--opt")
+                       .help("Choose if fit_mode or sir_mode"));
+  auto pippo_inzaghi = cli.parse({argc, argv});
   if (option == "sir") {
     double beta, gamma, S, I, R;
     int duration;
     cli.add_argument(lyra::opt(beta, "beta")
-		    .name("-b").name("--beta").help("Set the beta value"));
+                         .name("-b")
+                         .name("--beta")
+                         .help("Set the beta value"));
     cli.add_argument(lyra::opt(gamma, "gamma")
-		    .name("-g").name("--gamma").help("Set the gamma value"));
-    cli.add_argument(lyra::opt(S, "susceptibles")
-		    .name("-S").name("--susceptibles").help("Set the number of susceptibles at the start of the pandemic"));
-    cli.add_argument(lyra::opt(I, "infected")
-		    .name("-I").name("--infected").help("Set the number of infected at the start of the pandemic"));
-    cli.add_argument(lyra::opt(R, "removed")
-		    .name("-R").name("--removed").help("Set the number of removed at the start of the pandemic"));
+                         .name("-g")
+                         .name("--gamma")
+                         .help("Set the gamma value"));
+    cli.add_argument(
+        lyra::opt(S, "susceptibles")
+            .name("-S")
+            .name("--susceptibles")
+            .help(
+                "Set the number of susceptibles at the start of the pandemic"));
+    cli.add_argument(
+        lyra::opt(I, "infected")
+            .name("-I")
+            .name("--infected")
+            .help("Set the number of infected at the start of the pandemic"));
+    cli.add_argument(
+        lyra::opt(R, "removed")
+            .name("-R")
+            .name("--removed")
+            .help("Set the number of removed at the start of the pandemic"));
     cli.add_argument(lyra::opt(duration, "duration")
-		    .name("-d").name("--duration").help("Set the duration of the simulation"));
+                         .name("-d")
+                         .name("--duration")
+                         .help("Set the duration of the simulation"));
     auto pippo_franco = cli.parse({argc, argv});
     if (!pippo_franco) {
-	    std::cerr << "Error in command line: " << pippo_franco.errorMessage() << '\n';
-	    return 1;
+      std::cerr << "Error in command line: " << pippo_franco.errorMessage()
+                << '\n';
+      return 1;
     }
     std::cout << '\n';
     State initial_state{S, I, R};
@@ -45,11 +61,14 @@ int main(int argc, const char** argv) {
   } else if (option == "fit") {
     std::string data_file;
     cli.add_argument(lyra::opt(data_file, "data_file")
-		    .name("-f").name("--file").help("Select the file containing the data to fit"));
+                         .name("-f")
+                         .name("--file")
+                         .help("Select the file containing the data to fit"));
     auto pippo_baudo = cli.parse({argc, argv});
     if (!pippo_baudo) {
-	    std::cerr << "Error in command line: " << pippo_baudo.errorMessage() << '\n';
-	    return 1;
+      std::cerr << "Error in command line: " << pippo_baudo.errorMessage()
+                << '\n';
+      return 1;
     }
     Pandemy pandemy;
     auto const pandemic_data = pandemy.get_data(data_file);
--- mandatory_sir_model/pandemy.test.cpp
+++ mandatory_sir_model/pandemy.test.cpp
@@ -1,62 +1,62 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
-#include "Pandemy.hpp"
 #include "Least_Squares.hpp"
+#include "Pandemy.hpp"
 #include "doctest.h"
 
 TEST_CASE("TEST PROGRESSION N. 1") {
-    State default_state{};
-    Virus default_virus{};
-    Pandemy default_pandemy;
-    auto const progression = default_pandemy.progression(11);
-    CHECK(default_state.S == 900.);
-    CHECK(default_state.I == 100.);
-    CHECK(default_state.R == 0.);
-    CHECK(default_state.i_sigma == doctest::Approx(9.4868));
-    CHECK(default_virus.beta == 0.8);
-    CHECK(default_virus.gamma == 0.5);
-    CHECK(default_pandemy.get_state() == default_state);
-    CHECK(default_pandemy.get_virus() == default_virus);
-    for (int i = 0; i != 11; ++i) {
-      CHECK(doctest::Approx(progression[i].N) == default_state.N);
-    }
-    CHECK(progression[0].S == doctest::Approx(900.));
-    CHECK(progression[0].I == doctest::Approx(100.));
-    CHECK(progression[0].R == doctest::Approx(0.));
-    CHECK(progression[1].S == doctest::Approx(828.));
-    CHECK(progression[1].I == doctest::Approx(122.));
-    CHECK(progression[1].R == doctest::Approx(50.));
-    CHECK(progression[2].S == doctest::Approx(747.1872));
-    CHECK(progression[2].I == doctest::Approx(141.8128));
-    CHECK(progression[2].R == doctest::Approx(111.));
-    CHECK(progression[3].S == doctest::Approx(662.418632));
-    CHECK(progression[3].I == doctest::Approx(155.674967));
-    CHECK(progression[3].R == doctest::Approx(181.9064));
-    CHECK(progression[4].S == doctest::Approx(579.9210337));
-    CHECK(progression[4].I == doctest::Approx(160.3350827));
-    CHECK(progression[4].R == doctest::Approx(259.7438836));
-    CHECK(progression[5].S == doctest::Approx(505.535684));
-    CHECK(progression[5].I == doctest::Approx(154.5528909));
-    CHECK(progression[5].R == doctest::Approx(339.9114249));
-    CHECK(progression[6].S == doctest::Approx(443.030083));
-    CHECK(progression[6].I == doctest::Approx(139.7820466));
-    CHECK(progression[6].R == doctest::Approx(417.1878704));
-    CHECK(progression[7].S == doctest::Approx(393.4879617));
-    CHECK(progression[7].I == doctest::Approx(119.4341447));
-    CHECK(progression[7].R == doctest::Approx(487.0788937));
-    CHECK(progression[8].S == doctest::Approx(355.8915579));
-    CHECK(progression[8].I == doctest::Approx(97.31297605));
-    CHECK(progression[8].R == doctest::Approx(546.795466));
-    CHECK(progression[9].S == doctest::Approx(328.1852646));
-    CHECK(progression[9].I == doctest::Approx(76.36278135));
-    CHECK(progression[9].R == doctest::Approx(595.451954));
-    CHECK(progression[10].S == doctest::Approx(308.1363529));
-    CHECK(progression[10].I == doctest::Approx(58.23030236));
-    CHECK(progression[10].R == doctest::Approx(633.6333447));
+  State default_state{};
+  Virus default_virus{};
+  Pandemy default_pandemy;
+  auto const progression = default_pandemy.progression(11);
+  CHECK(default_state.S == 900.);
+  CHECK(default_state.I == 100.);
+  CHECK(default_state.R == 0.);
+  CHECK(default_state.i_sigma == doctest::Approx(9.4868));
+  CHECK(default_virus.beta == 0.8);
+  CHECK(default_virus.gamma == 0.5);
+  CHECK(default_pandemy.get_state() == default_state);
+  CHECK(default_pandemy.get_virus() == default_virus);
+  for (int i = 0; i != 11; ++i) {
+    CHECK(doctest::Approx(progression[i].N) == default_state.N);
+  }
+  CHECK(progression[0].S == doctest::Approx(900.));
+  CHECK(progression[0].I == doctest::Approx(100.));
+  CHECK(progression[0].R == doctest::Approx(0.));
+  CHECK(progression[1].S == doctest::Approx(828.));
+  CHECK(progression[1].I == doctest::Approx(122.));
+  CHECK(progression[1].R == doctest::Approx(50.));
+  CHECK(progression[2].S == doctest::Approx(747.1872));
+  CHECK(progression[2].I == doctest::Approx(141.8128));
+  CHECK(progression[2].R == doctest::Approx(111.));
+  CHECK(progression[3].S == doctest::Approx(662.418632));
+  CHECK(progression[3].I == doctest::Approx(155.674967));
+  CHECK(progression[3].R == doctest::Approx(181.9064));
+  CHECK(progression[4].S == doctest::Approx(579.9210337));
+  CHECK(progression[4].I == doctest::Approx(160.3350827));
+  CHECK(progression[4].R == doctest::Approx(259.7438836));
+  CHECK(progression[5].S == doctest::Approx(505.535684));
+  CHECK(progression[5].I == doctest::Approx(154.5528909));
+  CHECK(progression[5].R == doctest::Approx(339.9114249));
+  CHECK(progression[6].S == doctest::Approx(443.030083));
+  CHECK(progression[6].I == doctest::Approx(139.7820466));
+  CHECK(progression[6].R == doctest::Approx(417.1878704));
+  CHECK(progression[7].S == doctest::Approx(393.4879617));
+  CHECK(progression[7].I == doctest::Approx(119.4341447));
+  CHECK(progression[7].R == doctest::Approx(487.0788937));
+  CHECK(progression[8].S == doctest::Approx(355.8915579));
+  CHECK(progression[8].I == doctest::Approx(97.31297605));
+  CHECK(progression[8].R == doctest::Approx(546.795466));
+  CHECK(progression[9].S == doctest::Approx(328.1852646));
+  CHECK(progression[9].I == doctest::Approx(76.36278135));
+  CHECK(progression[9].R == doctest::Approx(595.451954));
+  CHECK(progression[10].S == doctest::Approx(308.1363529));
+  CHECK(progression[10].I == doctest::Approx(58.23030236));
+  CHECK(progression[10].R == doctest::Approx(633.6333447));
 }
 TEST_CASE("TEST FITTING N. 1") {
-	Pandemy default_pandemy;
-	Virus default_virus{};
-	auto const data_to_fit = default_pandemy.progression(200);
-	Virus computed_virus = get_parameters(data_to_fit);
-	CHECK(computed_virus == default_virus);
+  Pandemy default_pandemy;
+  Virus default_virus{};
+  auto const data_to_fit = default_pandemy.progression(200);
+  Virus computed_virus = get_parameters(data_to_fit);
+  CHECK(computed_virus == default_virus);
 }
