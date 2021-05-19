223a1a725f035c27539b8aedb3efc7cdd9c3f38a
Giuseppe Sguera <giuseppe.sguera2@studio.unibo.it>
2021-05-20 00:07:41 +0200

    added Print.cpp

---
+++ mandatory_sir_model/Print.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include "Pandemy.hpp"
+#include "Print.hpp"
+
+void print(std::vector<State> const& result) {
+  int day = 1;
+  for (auto& state : result) {
+    std::cout << "*--------------------------------------------------*" << '\n';
+    std::cout << "Day = " << day << "S = " << state.S << " I = " << state.I
+              << " R = " << state.R << '\n';
+    ++day;
+  }
+}
+
+void print_simple(std::vector<State> const& result) {
+  int day = 1;
+  for (auto& state : result) {
+    std::cout << day << " " << state.S << " " << state.I << " " << state.R
+              << '\n';
+    ++day;
+  }
+}
--- mandatory_sir_model/Print.hpp
+++ mandatory_sir_model/Print.hpp
@@ -1,23 +1,6 @@
 #ifndef PRINT_HPP
 #define PRINT_HPP
-#include <fstream>
 
-void print(std::vector<State> const& result) {
-  int day = 1;
-  for (auto& state : result) {
-    std::cout << "*--------------------------------------------------*" << '\n';
-    std::cout << "Day = " << day << "S = " << state.S << " I = " << state.I
-              << " R = " << state.R << '\n';
-    ++day;
-  }
-}
-
-void print_simple(std::vector<State> const& result) {
-  int day = 1;
-  for (auto& state : result) {
-    std::cout << day << " " << state.S << " " << state.I << " " << state.R
-              << '\n';
-    ++day;
-  }
-}
+void print(std::vector<State> const& result); 
+void print_simple(std::vector<State> const& result);
 #endif
