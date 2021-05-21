87c37c49cb2a2817f656fd69a07e99425a48e360
Giuseppe Sguera <giuseppe.sguera2@studio.unibo.it>
2021-05-21 18:10:33 +0200

    added constructor conditions

--- mandatory_sir_model/State.hpp
+++ mandatory_sir_model/State.hpp
@@ -1,12 +1,17 @@
 #ifndef STATE_HPP
 #define STATE_HPP
 #include <cmath>
+#include <stdexcept>
 struct State {
   double S;
   double I;
   double R;
   State(double susceptibles = 900., double infected = 100., double removed = 0.)
-      : S(susceptibles), I(infected), R(removed) {}
+      : S(susceptibles), I(infected), R(removed) {
+      if (S <= 0. || I <= 0. || R <= 0.) {
+      throw std::range_error("S, I, R must be positive values");
+      }
+      }
   double N = S + I + R;
   double i_sigma = std::sqrt(I * (N - I) / N);
 };
--- mandatory_sir_model/Virus.hpp
+++ mandatory_sir_model/Virus.hpp
@@ -1,9 +1,14 @@
 #ifndef VIRUS_HPP
 #define VIRSU_HPP
+#include <stdexcept>
 struct Virus {
   double beta;
   double gamma;
-  Virus(double beta = 0.8, double gamma = 0.5) : beta(beta), gamma(gamma) {}
+  Virus(double beta = 0.8, double gamma = 0.5) : beta(beta), gamma(gamma) {
+  if (beta < 0. || beta > 1. || gamma < 0. || gamma > 1.) {
+  throw std::range_error("Beta and gamma must be within the range [0.; 1.]");
+  }
+  }
 };
 bool operator==(Virus const& left, Virus const& right);
 #endif
