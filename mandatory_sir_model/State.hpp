2c755d581dda697f385152193ddf78bc0a0216eb
Giuseppe Sguera <giuseppe.sguera2@studio.unibo.it>
2021-05-21 23:33:09 +0200

    formatted

--- mandatory_sir_model/State.hpp
+++ mandatory_sir_model/State.hpp
@@ -8,10 +8,10 @@ struct State {
   double R;
   State(double susceptibles = 900., double infected = 100., double removed = 0.)
       : S(susceptibles), I(infected), R(removed) {
-      if (S < 0. || I < 0. || R < 0.) {
+    if (S < 0. || I < 0. || R < 0.) {
       throw std::range_error("S, I, R must be positive values");
-      }
-      }
+    }
+  }
   double N = S + I + R;
   double i_sigma = std::sqrt(I * (N - I) / N);
 };
--- mandatory_sir_model/Virus.hpp
+++ mandatory_sir_model/Virus.hpp
@@ -5,9 +5,10 @@ struct Virus {
   double beta;
   double gamma;
   Virus(double beta = 0.8, double gamma = 0.5) : beta(beta), gamma(gamma) {
-  if (beta < 0. || beta > 1. || gamma < 0. || gamma > 1.) {
-  throw std::range_error("Beta and gamma must be within the range [0.; 1.]");
-  }
+    if (beta < 0. || beta > 1. || gamma < 0. || gamma > 1.) {
+      throw std::range_error(
+          "Beta and gamma must be within the range [0.; 1.]");
+    }
   }
 };
 bool operator==(Virus const& left, Virus const& right);
