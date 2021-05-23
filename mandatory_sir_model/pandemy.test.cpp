4f88a4dbfadd672491372d8c528882919236558d
Giuseppe Sguera <giuseppe.sguera2@studio.unibo.it>
2021-05-23 19:48:30 +0200

    nothing

--- mandatory_sir_model/pandemy.test.cpp
+++ mandatory_sir_model/pandemy.test.cpp
@@ -45,7 +45,3 @@ TEST_CASE("TEST PROGRESSION N. 2") {
     CHECK(doctest::Approx(progression[i].N) == initial_state.N);
   }
 }
-// TEST_CASE("TESTING THROWS") {
-//	State invalid_state{};
-//	CHECK_THROWS(invalid_state{-270., 100., 0.});
-//}
