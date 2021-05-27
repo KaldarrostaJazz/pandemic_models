fb38e28ac0575cec819c583fa411f057e6fa819d
Giuseppe Sguera <giuseppe.sguera2@studio.unibo.it>
2021-05-27 11:23:25 +0200

    added ad intial version of print_fitting

--- mandatory_sir_model/Print.hpp
+++ mandatory_sir_model/Print.hpp
@@ -4,4 +4,5 @@
 
 void print(std::vector<State> const& result);
 void print_simple(std::vector<State> const& result);
+void print_fitting(std::vector<State> const& data, std::vector<State> const& fit);
 #endif
